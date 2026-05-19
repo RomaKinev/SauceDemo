720a082ac42d882cc5b104513f37612dcc84683c
romankinev <romakinev1@gmail.com>
2026-05-19 23:22:35 +0300

    домашка

--- src/test/java/listners/TestListener.java
+++ src/test/java/listners/TestListener.java
@@ -1,4 +1,4 @@
-package tests;
+package listners;
 
 import org.testng.ITestContext;
 import org.testng.ITestListener;
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import listners.TestListener;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
