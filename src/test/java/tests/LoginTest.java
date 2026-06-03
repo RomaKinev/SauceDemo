c3f874b985d6d3d3e65767b35e863bc0b0787ea0
romankinev <romakinev1@gmail.com>
2026-06-03 21:28:20 +0300

    fix

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -2,8 +2,6 @@ package tests;
 
 import io.qameta.allure.*;
 import lombok.extern.log4j.Log4j2;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -11,8 +9,6 @@ import static org.testng.Assert.assertEquals;
 
 @Log4j2
 public class LoginTest extends BaseTest {
-
-    private static final Logger log = LoggerFactory.getLogger(LoginTest.class);
 
     @Test(priority = 1,
             description = "Проверка логина с позитивными кредами",
